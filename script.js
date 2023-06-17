da79abdab53ccbf3b86b8a37589e268616b889fb
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2023-06-17 13:41:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// Fetch autocomplete suggestions from Spotify API
-async function fetchAutocompleteSuggestions(query) {
-  // Code for fetching autocomplete suggestions from Spotify API...
-}
-
-// Get the correct answer from Spotify API track name
-async function getCorrectAnswer() {
-  // Code for getting the correct answer from Spotify API...
-}
-
-// Handle instructions popup
+// Instructions popup functionality
 const instructionsButton = document.getElementById("instructions-button");
 const instructionsPopup = document.getElementById("instructions-popup");
-const popupCloseButton = document.getElementById("popup-close-button");
 const instructionOverlay = document.getElementById("instruction-overlay");
+const popupCloseButton = document.getElementById("popup-close-button");
 
 instructionsButton.addEventListener("click", () => {
   instructionsPopup.style.display = "block";
@@ -29,71 +19,70 @@ instructionOverlay.addEventListener("click", () => {
   instructionOverlay.style.display = "none";
 });
 
-// Handle user input and autocomplete functionality
-const userInput = document.getElementById("sound-name-input");
-const autocompleteDropdown = document.getElementById("autocomplete-dropdown");
-let correctAnswer = "";
-let triesLeft = 5;
-
-// Get the correct answer from the Spotify API
-getCorrectAnswer().then((answer) => {
-  correctAnswer = answer;
-});
-
-userInput.addEventListener("input", async (event) => {
-  const query = event.target.value.trim();
-  if (query.length === 0) {
-    autocompleteDropdown.innerHTML = ""; // Clear dropdown when input is empty
-    return;
-  }
-
-  const suggestions = await fetchAutocompleteSuggestions(query);
-  autocompleteDropdown.innerHTML = ""; // Clear previous suggestions
-
-  // Display suggestions in the dropdown
-  suggestions.forEach((suggestion) => {
-    const option = document.createElement("option");
-    option.value = suggestion;
-    autocompleteDropdown.appendChild(option);
-  });
-});
-
-userInput.addEventListener("change", (event) => {
-  const selectedValue = event.target.value.trim();
-
-  if (selectedValue.toLowerCase() === correctAnswer.toLowerCase()) {
-    document.getElementById("answer-text").textContent =
-      "You got it! The answer was the song name.";
-    document
-      .querySelector(".try-square:nth-child(" + triesLeft + ")")
-      .classList.add("correct");
-    userInput.disabled = true;
-    document.getElementById("submit-button").disabled = true;
-  } else {
-    document.getElementById("answer-text").textContent = "Sorry, try again.";
-    document
-      .querySelector(".try-square:nth-child(" + triesLeft + ")")
-      .classList.add("wrong");
-  }
-
-  triesLeft--;
-  if (triesLeft === 0) {
-    userInput.disabled = true;
-    document.getElementById("submit-button").disabled = true;
-  }
-});
-
-// Styling for the answer message
-const answerText = document.getElementById("answer-text");
-answerText.style.color = "white";
-
-// Styling for the tries squares
-const triesSquares = document.querySelectorAll(".try-square");
-triesSquares.forEach((square) => {
-  square.style.backgroundColor = "lightgrey";
-  square.style.borderRadius = "5px";
-});
-
-// Styling for the SoundCloud player
+// SoundCloud player initialization
+// Replace 'YOUR_SOUND_CLOUD_TRACK_URL' with the actual SoundCloud track URL
 const soundCloudPlayer = document.querySelector("iframe");
-soundCloudPlayer.style.marginBottom = "20px";
+const soundCloudURL = "YOUR_SOUND_CLOUD_TRACK_URL";
+soundCloudPlayer.src = `https://w.soundcloud.com/player/?url=${soundCloudURL}`;
+
+// Track name submission functionality
+const userInput = document.getElementById("sound-name-input");
+const submitButton = document.getElementById("submit-button");
+const answerText = document.getElementById("answer-text");
+const triesSquares = document.querySelectorAll(".try-square");
+const maxTries = triesSquares.length;
+let triesLeft = maxTries;
+
+submitButton.addEventListener("click", () => {
+  const userAnswer = userInput.value.toLowerCase();
+  const correctAnswer = "your_correct_answer";
+
+  if (userAnswer === correctAnswer) {
+    answerText.textContent = "You got it - The answer was the song name";
+    triesSquares[maxTries - triesLeft].classList.add("correct");
+    userInput.disabled = true;
+    submitButton.disabled = true;
+
+    // Calculate time
+    const startTime = soundCloudPlayer.contentWindow.SC.Widget.Events.PLAY;
+    const endTime = new Date().getTime();
+    const timeDiff = Math.abs(endTime - startTime);
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    const timerText = document.getElementById("timer");
+    timerText.textContent = `Time taken: ${seconds} seconds`;
+  } else {
+    answerText.textContent = "Wrong answer. Try again.";
+    triesSquares[maxTries - triesLeft].classList.add("wrong");
+    triesLeft--;
+
+    if (triesLeft === 0) {
+      userInput.disabled = true;
+      submitButton.disabled = true;
+      answerText.textContent = "Out of tries. The answer was the song name.";
+    }
+  }
+
+  userInput.value = "";
+});
+
+// Autocomplete functionality using Spotify API
+const autocompleteDropdown = document.getElementById("autocomplete-dropdown");
+userInput.addEventListener("input", () => {
+  const query = userInput.value.toLowerCase();
+  autocompleteDropdown.innerHTML = "";
+
+  // Make API call to Spotify API for autocomplete suggestions
+  // Replace 'YOUR_SPOTIFY_API_ENDPOINT' with the actual Spotify API endpoint
+  fetch(`YOUR_SPOTIFY_API_ENDPOINT?q=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const tracks = data.tracks.items;
+
+      tracks.forEach((track) => {
+        const option = document.createElement("option");
+        option.value = track.name;
+        autocompleteDropdown.appendChild(option);
+      });
+    });
+});
